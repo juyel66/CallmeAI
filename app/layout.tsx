2ad0e8725422c14781113923d8948848b95a4b08
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-03-29 05:58:52 +0600

    fix: format layout.tsx for better readability

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
 
-      
-      <body className="min-h-full flex flex-col">{children}</body>
+
+      <body className="min-h-full flex flex-col">
+        {children}
+
+      </body>
     </html>
   );
 }
