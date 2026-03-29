0429af8a4693aaca630953f61806e759fd438aa3
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-03-30 05:32:53 +0600

    feat: add PrivacyPolicy page component

---
+++ app/(WIthCommonLayout)/privacy-policy/page.tsx
@@ -0,0 +1,13 @@
+import PrivacyPolicy from "@/app/pages/PrivacyPolicy/PrivacyPolicy";
+
+
+const page = () => {
+    return (
+        <div>
+            <PrivacyPolicy />
+            
+        </div>
+    );
+};
+
+export default page;
