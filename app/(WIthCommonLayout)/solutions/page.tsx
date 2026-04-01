696340e0bba7ae15ef8c74af2246a0b3c8292d94
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-01 06:35:45 +0600

    feat: refactor Solutions component structure and enhance layout with content and styling

--- app/(WIthCommonLayout)/solutions/page.tsx
+++ app/(WIthCommonLayout)/solutions/page.tsx
@@ -1,14 +1,65 @@
+import HowItWork from "@/app/pages/HowItWork/HowItWork";
 import InfiniteSlider from "@/app/pages/Slider/InfiniteSlider";
+import Buttons from "@/app/Shared/Button/Buttons";
+import Image from "next/image";
+import React from "react";
 
 
-const page = () => {
-    return (
-        <div>
-        
-            <InfiniteSlider />
-            
+const Solutions: React.FC = () => {
+
+
+  return (
+    <>
+      <section className="bg-[#F5F6F8] py-16">
+        <div className="container  mx-auto px-6 flex flex-col lg:flex-row items-center justify-between gap-6">
+
+        {/* LEFT CONTENT */}
+        <div className="max-w-xl">
+
+          <h1 className="text-[36px] leading-[1.2] font-bold text-black">
+           Boost Your Sales with AI-Powered Appointment Setting on Autopilot
+          </h1>
+
+          <p className="mt-4 text-[15px] text-gray-600 leading-[1.6]">
+            Maximize revenue potential with CallmeAI. Our AI phone agents
+            instantly contact new leads, follow up automatically, and book
+            high-quality appointments on autopilot.
+          </p>
+
+          {/* BUTTONS */}
+       <div className="mt-8">
+         <Buttons />
+       </div>
+
         </div>
-    );
+
+        {/* RIGHT IMAGE */}
+        <div className="relative">
+          <div className="rounded-2xl overflow-hidden   shadow-lg">
+            <Image
+              src="/Rectangle 7.svg"
+              alt="AI Dashboard"
+              className="object-cover"
+                width={480}
+                height={280}
+            />
+            
+          </div>
+
+          {/* Glow effect */}
+          <div className="absolute inset-0 rounded-2xl border border-purple-400 opacity-30 blur-sm"></div>
+        </div>
+
+        </div>
+      </section>
+
+      <InfiniteSlider />
+
+      <HowItWork />
+
+      
+    </>
+  );
 };
 
-export default page;
+export default Solutions;
