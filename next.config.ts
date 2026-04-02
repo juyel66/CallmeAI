6ea932cb0eda078822366ac3c0ea07a5dd29b95e
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-02 07:04:09 +0600

    feat: update next.config.ts to include remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["172.21.224.1", "10.10.13.88"],
+ 
 
-    images: {
-    domains: ["images.unsplash.com"],
-  },
+  images: {
+    
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+
   
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+  
+
+    ],
+  },
 };
 
 export default nextConfig;
