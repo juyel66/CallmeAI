38f4c15ba1cd67b98614d2ce5a30dcef0fc8589f
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-02 07:04:21 +0600

    feat: add HowItWork, AIJustGot, and AiSolutions components to HomePage

--- app/(WIthCommonLayout)/page.tsx
+++ app/(WIthCommonLayout)/page.tsx
@@ -1,6 +1,9 @@
+import AIJustGot from "../pages/AIJustGot/AIJustGot";
+import AiSolutions from "../pages/AiSolutions/AiSolutions";
 import Connect from "../pages/HomePage/Connect";
 import HeroSection from "../pages/HomePage/HeroSection";
 import WhyEvery from "../pages/HomePage/WhyEvery";
+import HowItWork from "../pages/HowItWork/HowItWork";
 
 
 const HomePage = () => {
@@ -9,6 +12,9 @@ const HomePage = () => {
       <HeroSection />
       <WhyEvery />
       <Connect />
+      <HowItWork />
+      <AIJustGot />
+      <AiSolutions />
 
       
     </div>
