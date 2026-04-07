abc76be354b830df42db7106347f7190eacbaafc
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-08 00:41:13 +0600

    Update SolutionsHeroPages component: change image source and adjust text margins for improved layout

--- app/pages/SolutionPage/SolutionsHero/SolutionsHeroPages.tsx
+++ app/pages/SolutionPage/SolutionsHero/SolutionsHeroPages.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import groupImage from "../../../../public/images/Group 14.svg";
+import groupImage from "../../../../public/images/Group 14 (1).svg";
 import Link from "next/link";
 
 const SolutionsHeroPages = () => {
@@ -9,13 +9,13 @@ const SolutionsHeroPages = () => {
         <section className="w-full py-10 transition-all duration-300 sm:py-12 md:py-20 lg:py-24">
             <div className="container mx-auto grid items-center gap-8 px-4 transition-all duration-300 sm:px-6 md:grid-cols-2 md:gap-10 lg:gap-12 lg:px-8">
 
-                {/* LEFT CONTENT */}
+               
                 <div className="min-w-0 max-w-xl transition-all duration-300 lg:max-w-none flex flex-col items-start text-left">
-                    <h1 className="text-[clamp(2rem,5vw,3.5rem)] font-bold leading-tight text-black transition-all duration-300">
+                    <h1 className="md:text-[46px] text-2xl font-bold leading-tight text-black transition-all duration-300">
                         Boost Your Sales with AI-Powered Appointment Setting on Autopilot.
                     </h1>
 
-                    <p className="mt-5 max-w-prose text-sm leading-relaxed text-[#444] transition-all duration-300 sm:mt-6 sm:text-base md:text-lg">
+                    <p className="mt-2 max-w-prose text-sm leading-relaxed  text-[#444] transition-all duration-300 sm:mt-6 sm:text-base md:text-[20px] ">
                         Optimize your sales process with automated calling, multi-layer follow up, and smart qualification. Reach more leads, book more appointments, and close more deals.
                     </p>
 
@@ -40,7 +40,7 @@ const SolutionsHeroPages = () => {
                     </Link>
                 </div>
 
-                {/* RIGHT IMAGE AREA */}
+             
                 <div className="relative flex min-w-0 items-center justify-center transition-all duration-300 md:justify-self-end">
                     <Image
                         src={groupImage}
