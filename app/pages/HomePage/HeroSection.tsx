fce62e4e5746709fbfc201e59235e91df20a2239
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-04-09 04:48:10 +0600

    Refactor HeroSection component: restructure layout and enhance mobile responsiveness

--- app/pages/HomePage/HeroSection.tsx
+++ app/pages/HomePage/HeroSection.tsx
@@ -5,7 +5,8 @@ import Buttons from "@/app/Shared/Button/Buttons";
 
 const HeroSection = () => {
   return (
-    <section className="relative flex min-h-[78svh] w-full items-center justify-center overflow-hidden px-4 py-8 sm:min-h-[85vh] sm:px-6 sm:py-14 lg:min-h-[89vh] lg:py-16">
+   <div>
+     <section className="relative md:flex hidden min-h-[78svh] w-full items-center justify-center overflow-hidden px-4 py-8 sm:min-h-[85vh] sm:px-6 sm:py-14 lg:min-h-[89vh] lg:py-16">
       <Image
         src={heroSectionsSmallDevice}
         alt="Hero Background Mobile"
@@ -28,8 +29,8 @@ const HeroSection = () => {
 
       <div className="absolute inset-0 bg-linear-to-b"></div>
 
-      <div className="relative z-10 w-full container mx-auto">
-        <div className="rounded-2xl border border-white/20 bg-white/4 p-4 text-center shadow-lg backdrop-blur-[1px] sm:p-8 md:p-12">
+      <div className=" relative z-10 w-full container mx-auto">
+        <div className="rounded-2xl border border-white/20 bg-white/4 p-4 text-center shadow-lg backdrop-blur-[2px] sm:p-8 md:p-12">
           <h1 className="mb-4 text-xl font-bold leading-tight text-white sm:text-4xl md:text-5xl">
             Automated Lead Contact, Follow-Up & <br /> Prequalified Bookings — Straight to Your <br /> Calendar
           </h1>
@@ -45,6 +46,49 @@ const HeroSection = () => {
         </div>
       </div>
     </section>
+
+     {/* mobile device   */}
+     <section className="relative flex md:hidden lg:hidden min-h-[78svh] w-full items-center justify-center overflow-hidden px-4 py-8 sm:min-h-[85vh] sm:px-6 sm:py-14 lg:min-h-[89vh] lg:py-16">
+      <Image
+        src={heroSectionsSmallDevice}
+        alt="Hero Background Mobile"
+        fill
+        quality={100}
+        priority
+        className="object-cover object-bottom md:hidden"
+        sizes="100vw"
+      />
+
+      <Image
+        src={heroSections}
+        alt="Hero Background"
+        fill
+        quality={100}
+        priority
+        className="hidden object-cover object-center md:block"
+        sizes="100vw"
+      />
+
+      <div className="absolute inset-0 bg-linear-to-b"></div>
+
+      <div className=" relative z-10 w-full container mx-auto">
+        <div className="rounded-2xl border border-white/20 bg-white/4 p-4 text-center shadow-lg backdrop-blur-[1px] sm:p-8 md:p-12">
+          <h1 className="mb-4 text-xl font-bold leading-tight text-white sm:text-4xl md:text-5xl">
+            Automated Lead Contact, Follow-Up &  Prequalified Bookings — Straight to Your  Calendar
+          </h1>
+
+          <p className="mx-auto mb-7 max-w-3xl px-1 text-sm leading-relaxed text-white sm:mb-8 sm:text-base md:text-lg">
+            Maximize revenue potential with CallmeAI. Our AI phone agents instantly contact new 
+            leads, follow up automatically, and book high-quality appointments on autopilot.
+          </p>
+
+          <div className="mx-auto w-full max-w-md sm:max-w-none">
+            <Buttons />
+          </div>
+        </div>
+      </div>
+    </section>
+   </div>
   );
 };
 
